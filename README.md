53f05696cdf52fd202442090dc9f3dd9ceb43df7
Andrew William Kaskiw <30780132+kaskiw@users.noreply.github.com>
2018-04-17 19:37:54 -0400

    started the react app and compiled some dependencies

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ppd
 PPD is puppy play date. It is a Meetup like app (using react) that gets dogs and their owners to meet new families and build life long friendships.
---
+++ src/gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// dependencies //
+
+const gulp = require ("gulp");
+const sass = require ("gulp-sass");
+const minifyCSS = require ("gulp-clean-css");
+const uglify = require ("gulp-uglify");
+const rename = require ("gulp-rename");
+const changed = require ("gulp-changed");
+
+
+/////////
+// SCSS - CSS
+/////////
+
+const SCSS_SRC = './src/Assets/scss/**/*.scss';
+const SCSS_DEST = './src/Assets/scss/css';
+
+// compile scss
+ gulp.task('compile_scss', function(){
+     gulp.src(SCSS_SRC)
+     .pipe(sass().on('err', sass.logError))
+     .pipe(minifyCSS())
+     .pipe(rename({ suffix: '.min'}))
+     .pipe(changed(SCSS_DEST))
+     .pipe(gulp.dest(SCSS_DEST));
+ });
+
+ // alert changes in SCSS
+ gulp.task('watch_scss', function () {
+    gulp.watch(SCSS_SRC, ['compile_scss']); 
+ });
+
+ // Run commands
+ gulp.task('defult', ['watch_scss']);
